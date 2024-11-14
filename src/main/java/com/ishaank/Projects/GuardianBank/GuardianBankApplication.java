f6a8a7c032dfb8dd7b41f98a795266032421cf1d
IshaankChopra <89826800+IshaankC@users.noreply.github.com>
2024-11-14 17:21:57 +0800

    start

---
+++ src/main/java/com/ishaank/Projects/GuardianBank/GuardianBankApplication.java
@@ -0,0 +1,13 @@
+package com.ishaank.Projects.GuardianBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuardianBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuardianBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ishaank/Projects/GuardianBank/GuardianBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishaank.Projects.GuardianBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuardianBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
